d115a64dd30914c3e303c8713abe1dcc982e9c80
thakurrohit27 <65273390+thakurrohit27@users.noreply.github.com>
2024-02-05 20:02:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+const sr = ScrollReveal(
+    {
+        distance:'60px',
+        duration:2500,
+        delay:400,
+        reset:true
+    }
+)
+
+sr.reveal('.home-text',{delay:200,origin:'top'})
+sr.reveal('.ride-heading',{delay:800,origin:'top'})
+sr.reveal('.ride-container .box',{delay:600,origin:'top'})
+sr.reveal('.search-tour form',{delay:600,origin:'left'})
+sr.reveal('.services-heading',{delay:800,origin:'top'})
+sr.reveal('.services-container',{delay:600,origin:'right'})
+sr.reveal('.review-heading',{delay:800,origin:'top'})
+sr.reveal('.review-container',{delay:600,origin:'bottom'})
+sr.reveal('.contact-heading',{delay:800,origin:'top'})
+sr.reveal('.contact-container',{delay:600,origin:'bottom'})
